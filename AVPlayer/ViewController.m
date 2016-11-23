57a03dc20511feaee49d42f8b5654f261794e648
YinSQ <18620895340@163.com>
2016-11-23 10:30:41 +0800

    Initial Commit

---
+++ AVPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayer
+//
+//  Created by 尹思迁 on 2016/11/23.
+//  Copyright © 2016年 尹思迁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
